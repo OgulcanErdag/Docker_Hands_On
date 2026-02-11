56265fea234884ee7569e245f04295ea36775711
Ogulcan Erdag <ogulcanerdag@gmail.com>
2026-02-11 15:29:18 +0100

    docs(docker-01): update EC2 install template and Terraform setup

--- docker-01-installing-on-ec2-linux2/ec2-docker.tf
+++ docker-01-installing-on-ec2-linux2/ec2-docker.tf
@@ -2,22 +2,22 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "~> 5.0"
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
   }
 }
 
 
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
 locals {
-  instance-type = "t2.micro"
-  key-name = "mykey"
-  secgr-dynamic-ports = [22,80,443,8080,5000]
-  user = "myuser"
+  instance-type       = "t3.micro"
+  key-name            = "ogi-us-key"
+  secgr-dynamic-ports = [22, 80, 443, 8080, 5000]
+  user                = "myuser"
 }
 
 resource "aws_security_group" "allow_ssh" {
@@ -27,12 +27,12 @@ resource "aws_security_group" "allow_ssh" {
   dynamic "ingress" {
     for_each = local.secgr-dynamic-ports
     content {
-      from_port = ingress.value
-      to_port = ingress.value
-      protocol = "tcp"
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
+    }
   }
-}
 
   egress {
     description = "Outbound Allowed"
@@ -44,30 +44,30 @@ resource "aws_security_group" "allow_ssh" {
 }
 
 data "aws_ami" "al2023" {
-  most_recent      = true
-  owners           = ["amazon"]
+  most_recent = true
+  owners      = ["amazon"]
 
   filter {
-    name = "virtualization-type"
+    name   = "virtualization-type"
     values = ["hvm"]
   }
   filter {
-    name = "name"
+    name   = "name"
     values = ["al2023-ami-2023*"]
   }
   filter {
-    name = "architecture"
+    name   = "architecture"
     values = ["x86_64"]
   }
 }
 
 resource "aws_instance" "tf-ec2" {
-  ami           = data.aws_ami.al2023.id
-  instance_type = local.instance-type
-  key_name = local.key-name
-  vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
+  ami                    = data.aws_ami.al2023.id
+  instance_type          = local.instance-type
+  key_name               = local.key-name
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   tags = {
-      Name = "${local.user}-Docker-instance"
+    Name = "${local.user}-Docker-instance"
   }
 
   user_data = <<-EOF
@@ -82,7 +82,7 @@ resource "aws_instance" "tf-ec2" {
               curl -SL https://github.com/docker/compose/releases/download/v2.28.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
               chmod +x /usr/local/bin/docker-compose
 	          EOF
-}  
+}
 output "myec2-public-ip" {
   value = aws_instance.tf-ec2.public_ip
 }
